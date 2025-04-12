2afd6dd12b3166abb7ecea172d50e9d610b9ff3c
arpitsingh4297 <arpitsingh4297@outlook.com>
2025-04-13 05:29:02 +0530

    Merged remote changes and resolved conflicts

--- venv/Lib/site-packages/tensorflow/include/external/curl/lib/curl_get_line.h
+++ venv/Lib/site-packages/tensorflow/include/external/curl/lib/curl_get_line.h
@@ -7,7 +7,7 @@
  *                            | (__| |_| |  _ <| |___
  *                             \___|\___/|_| \_\_____|
  *
- * Copyright (C) Daniel Stenberg, <daniel@haxx.se>, et al.
+ * Copyright (C) 1998 - 2022, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
  * This software is licensed as described in the file COPYING, which
  * you should have received as part of this distribution. The terms
@@ -24,9 +24,8 @@
  *
  ***************************************************************************/
 
-#include "dynbuf.h"
-
-/* Curl_get_line() returns complete lines that end with a newline. */
-int Curl_get_line(struct dynbuf *buf, FILE *input);
+/* get_line() makes sure to only return complete whole lines that fit in 'len'
+ * bytes and end with a newline. */
+char *Curl_get_line(char *buf, int len, FILE *input);
 
 #endif /* HEADER_CURL_GET_LINE_H */
